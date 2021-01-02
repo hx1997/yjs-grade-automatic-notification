f40346c44c566cb1c7729d5572a5273f741c90b3
hx1997 <hx970923@gmail.com>
2021-01-02 19:48:33 +0800

    Adapt for use in HBU; Add WeChat notification feature

--- config_example.py
+++ config_example.py
@@ -1,12 +1,19 @@
-enable_mail = True  # 此行表示是否发送邮件提醒，设置为 True 发邮件
-smtp_server = 'smtp.163.com'  # 此行表示 smtp 服务器地址
-smtp_username = 'xxx@163.com'  # 此行表示发件人邮箱地址
+enable_mail = False  # 此行表示是否发送邮件提醒，设置为 True 发邮件
+smtp_server = 'smtp.qq.com'  # 此行表示 smtp 服务器地址
+smtp_username = 'xxx@qq.com'  # 此行表示发件人邮箱地址
 smtp_password = 'xxx'  # 此行表示发件人邮箱密码
-smtp_to = 'xxx@163.com'  # 此行表示收件人邮箱地址
+smtp_to = 'xxx@qq.com'  # 此行表示收件人邮箱地址
 smtp_ssl = True  # 此行表示连接邮箱是否使用 SSL
 
-student_no = 'SAxxxxxxxx'  # 此行表示登录统一身份认证系统的学号
+student_no = 'xxx'  # 此行表示登录统一身份认证系统的学号
 cas_password = 'xxx'  # 此行表示登录统一身份认证系统的密码
 
 req_timeout = 10  # 此行表示向服务器发查询请求的超时时间（单位：秒）
 interval = 60  # 此行表示两次查询的间隔时间（单位：秒）
+
+#是否开启微信通知新成绩功能，如果开启，必须填写下面的sckey
+enable_wechat = True
+#微信通知功能使用了Server酱提供的API
+#获取SCUKEY的方式见：http://sc.ftqq.com/3.version
+#按照网页上前两步指示“登入”和“绑定”做，然后把获得的SCKEY填入下一行
+sckey = 'xxx'
--- yjs_grade.py
+++ yjs_grade.py
@@ -5,73 +5,137 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+import shutil
 import config
+import muggle_ocr
+import json
+import re
 from mail import send_email
 
 s = requests.session()
 
+io_enabled = True
+def log(type, message, *args):
+    global io_enabled
+    if not io_enabled: return
+    if type == 'info':
+        logging.info(message, *args)
+    elif type == 'warning':
+        logging.warning(message, *args)
+    elif type == 'error':
+        logging.error(message, *args)
+    elif type == 'exception':
+        logging.exception(message, *args)
+
+def recognize(img_path):
+    sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
+    with open('img.jpg', 'rb') as f:
+        b = f.read()
+        text = sdk.predict(image_bytes=b)
+    return text
 
 def cas_login():
-    logging.info('Logging in')
-    url = 'https://passport.ustc.edu.cn/login'
-    r = s.post(url, data={
-        'model': 'uplogin.jsp',
-        'service': 'http://yjs.ustc.edu.cn/default.asp',
-        'warn': '',
-        'showCode': '',
-        'button': '',
+    log('info', 'Logging in')
+    url = 'http://202.206.3.95/login.do'
+    captcha = 'http://202.206.3.95/captcha.jpg'
+    s.get(url, timeout=config.req_timeout)
+    r = s.get(captcha, stream=True)
+    with open('img.jpg', 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+    try:
+        text = recognize('img.jpg')
+        log('info', text)
+    except ValueError:
+        return False
+    getDanDianInfo = 'http://202.206.3.95/getDanDianInfo.do'
+    r = s.post(getDanDianInfo, data={
+        'm': 'getLoginInfo',
         'username': config.student_no,
-        'password': config.cas_password
+        'pwd': config.cas_password
     }, timeout=config.req_timeout)
-    if 'yjs.ustc.edu.cn/main.asp' in r.url:
-        logging.info('Login success')
-        return True
+    DanDianInfo = r.content.decode('gbk')
+    if 'success' in DanDianInfo:
+        log('info', 'GetDanDianInfo success')
     else:
-        logging.error('Login failed')
+        log('error', 'GetDanDianInfo failed')
         return False
 
+    r_json = json.loads(DanDianInfo)
+    login_url = 'http://202.206.3.95/j_acegi_login.do'
+    r = s.post(login_url, data={
+        'm': 'getLoginInfo',
+        'j_username': config.student_no,
+        'j_password': r_json['password'],
+        'j_captcha_response': text,
+        'login': 'tongyi'
+    }, timeout=config.req_timeout)
+    if 'http://202.206.3.95/main.do' in r.url:
+        log('info', 'Login success')
+        return True
+    else:
+        log('error', 'Login failed')
+        return False
 
 def get_grade():
-    r = s.get('http://yjs.ustc.edu.cn/score/m_score.asp', timeout=config.req_timeout)
-    soup = BeautifulSoup(r.content, 'html5lib')
+    r = s.get('http://202.206.3.95/cjgl.v_allcj_yjs.do', timeout=config.req_timeout)
     grades = {}
-    for row in soup.find_all('table')[-1].find_all('tr')[1:]:
-        cols = row.find_all('td')
-        name = cols[3].text
-        grade = cols[4].text
-        grades[name] = grade
+    p = re.compile('var gridData = (.*?);', re.DOTALL)
+    m = p.findall(r.content.decode('gbk'))
+    raw_grades = json.loads(m[0])
+    for subject in raw_grades:
+        subject_name = subject[2]
+        subject_score = subject[4]
+        grades[subject_name] = subject_score
+    log('info', grades)
     return grades
 
+def send_wechat_notif(text):
+    server_url = 'https://sc.ftqq.com/{}.send'.format(config.sckey)
+    r = s.post(server_url, data={
+        'text': '新成绩',
+        'desp': text,
+    }, timeout=config.req_timeout)
+    if r.ok:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s')
     last_grades = {}
-    first_run = True
-    cas_login()
+    while not cas_login():
+        logging.info('Retrying login...')
+    logging.info('Fetching grades')
+
     while True:
         try:
             try:
-                logging.info('Fetching grades')
+                log('info', 'Testing I/O...')
+            except IOError:
+                io_enabled = False
+            try:
                 grades = get_grade()
-                logging.info('Count = %s', len(grades))
+                log('info', 'Count = %s', len(grades))
+                with open('log.txt', 'a') as f:
+                    f.write(str(grades))
+            except IOError:
+                pass
             except Exception as e:
-                logging.warning('Error occurred, trying to login again')
+                log('warning', 'Error occurred, trying to login again')
                 cas_login()
             else:
-                test_mail = False
-                if first_run:
-                    ans = input('Send test email? (y/n)')
-                    if ans.lower() in ['', 'y', 'yes']:
-                        test_mail = True
-                if (not first_run and grades != last_grades) or test_mail:
+                if grades != last_grades:
                     text = ', '.join(name + ' ' + grade for name, grade in grades.items() if name not in last_grades)
-                    logging.info('Sending email: %s', text)
+                    if config.enable_wechat:
+                        while not send_wechat_notif(text):
+                            log('warning', 'Failed to send WeChat notification, retrying...')
+                    log('info', 'Sending email: %s', text)
                     if config.enable_mail:
                         send_email(text, text)
-                        logging.info('Mail sent')
+                        log('info', 'Mail sent')
                 last_grades = grades
-                first_run = False
             finally:
                 time.sleep(config.interval)
         except Exception as e:
-            logging.exception(e)
+            log('exception', e)
